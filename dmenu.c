1e9d18db314a48bb2dca5f74875173ddce4a5997
nkitan <ankitdas2k@gmail.com>
2021-09-19 18:41:14 +0530

    update sept.

--- dmenu.c
+++ dmenu.c
@@ -89,6 +89,17 @@ calcoffsets(void)
 			break;
 }
 
+static int
+max_textw(void)
+{
+       int len = 0;
+       for (struct item *item = items; item && item->text; item++)
+               len = MAX(TEXTW(item->text), len);
+       return len;
+}
+
+
+
 static void
 cleanup(void)
 {
@@ -611,6 +622,7 @@ setup(void)
 	bh = drw->fonts->h + 2;
 	lines = MAX(lines, 0);
 	mh = (lines + 1) * bh;
+    promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
 #ifdef XINERAMA
 	i = 0;
 	if (parentwin == root && (info = XineramaQueryScreens(dpy, &n))) {
@@ -635,11 +647,19 @@ setup(void)
 		if (mon < 0 && !area && XQueryPointer(dpy, root, &dw, &dw, &x, &y, &di, &di, &du))
 			for (i = 0; i < n; i++)
 				if (INTERSECT(x, y, 1, 1, info[i]))
-					break;
+	                	break;
+              
+        if (centered) {
+                 mw = MIN(MAX(max_textw() + promptw, min_width), info[i].width);
+                 x = info[i].x_org + ((info[i].width  - mw) / 2);
+                 y = info[i].y_org + ((info[i].height - mh) / 2);
+         } else {
+                 x = info[i].x_org;
+                 y = info[i].y_org + (topbar ? 0 : info[i].height - mh);
+                 mw = info[i].width;
+         }
 
-		x = info[i].x_org;
-		y = info[i].y_org + (topbar ? 0 : info[i].height - mh);
-		mw = info[i].width;
+
 		XFree(info);
 	} else
 #endif
@@ -647,10 +667,18 @@ setup(void)
 		if (!XGetWindowAttributes(dpy, parentwin, &wa))
 			die("could not get embedding window attributes: 0x%lx",
 			    parentwin);
-		x = 0;
-		y = topbar ? 0 : wa.height - mh;
-		mw = wa.width;
-	}
+              
+             if (centered) {
+                     mw = MIN(MAX(max_textw() + promptw, min_width), wa.width);
+                     x = (wa.width  - mw) / 2;
+                     y = (wa.height - mh) / 2;
+             } else {
+                     x = 0;
+                     y = topbar ? 0 : wa.height - mh;
+                     mw = wa.width;
+             }
+
+    }
 	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
 	inputw = MIN(inputw, mw/3);
 	match();
@@ -659,9 +687,10 @@ setup(void)
 	swa.override_redirect = True;
 	swa.background_pixel = scheme[SchemeNorm][ColBg].pixel;
 	swa.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask;
-	win = XCreateWindow(dpy, parentwin, x, y, mw, mh, 0,
+	win = XCreateWindow(dpy, parentwin, x, y, mw, mh, border_width,
 	                    CopyFromParent, CopyFromParent, CopyFromParent,
 	                    CWOverrideRedirect | CWBackPixel | CWEventMask, &swa);
+    XSetWindowBorder(dpy, win, scheme[SchemeSel][ColBg].pixel);
 	XSetClassHint(dpy, win, &ch);
 
 
@@ -709,7 +738,10 @@ main(int argc, char *argv[])
 			topbar = 0;
 		else if (!strcmp(argv[i], "-f"))   /* grabs keyboard before reading stdin */
 			fast = 1;
-		else if (!strcmp(argv[i], "-i")) { /* case-insensitive item matching */
+	    else if(!strcmp(argv[i], "-c")){   /* centers dmenu on screen */
+            centered = 1;    
+        }
+    	else if (!strcmp(argv[i], "-i")) { /* case-insensitive item matching */
 			fstrncmp = strncasecmp;
 			fstrstr = cistrstr;
 		} else if (i + 1 == argc)
